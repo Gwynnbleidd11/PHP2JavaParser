a2b0877502b3a3b77c3da47d1cdac3ff8f1d83d8
Patryk Krzyżak <p.krzyzak1986@gmail.com>
2025-02-20 16:00:40 +0100

    HTML, CSS and JS added. JS not working correctly yet

--- src/main/java/com/application/controller/ParserController.java
+++ src/main/java/com/application/controller/ParserController.java
@@ -3,12 +3,12 @@ package com.application.controller;
 import com.application.service.ParserService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/php_parser")
 public class ParserController {
 
